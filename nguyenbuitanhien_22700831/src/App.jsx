edeab2a8a7187bab2b7f76b0eaeac12464e4a644
Colenviem <hiennguyenbuitan@gmail.com>
2025-04-10 21:28:50 +0700

    1. Layout Grid css

---
+++ nguyenbuitanhien_22700831/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+    return (
+      <div className='flex justify-center'>
+        <div className="container">
+          <div className="header">
+            <h2>My Header</h2>
+          </div>
+    
+          <div className="menu">
+            <a href="#">Link 1</a>
+            <br />
+            <a href="#">Link 2</a>
+            <br />
+            <a href="#">Link 3</a>
+          </div>
+          
+          <div className="content">
+            <h3>content</h3>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default App
