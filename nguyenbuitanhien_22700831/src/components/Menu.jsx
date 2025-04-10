dba73403176ccc1d65e1b339a0d6cdc406fe22ea
Colenviem <hiennguyenbuitan@gmail.com>
2025-04-10 22:27:16 +0700

    6. isActive (NavLink React Router)

--- nguyenbuitanhien_22700831/src/components/Menu.jsx
+++ nguyenbuitanhien_22700831/src/components/Menu.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Logo from '../assets/images/Image 1858.png'
+import Group from '../assets/images/Group.png'
 
 const Menu = ( {listMenu} ) => {
     return (
+        <div className='sidebar'>
+            <div className="image">
+                <img src={Logo} alt="Logo" />
+            </div>
+            
             <div className="menu">
                 {listMenu.map((menuItem) => {
                     return (
-                        <div key={menuItem.id}>
-                            {/* <img src={menuItem.image} alt={menuItem.menuName} /> */}
-                            <h3>{menuItem.menuName}</h3>
-                        </div>
+                        <NavLink 
+                            key={menuItem.id}
+                            to={menuItem.path}
+                            className={({ isActive }) => isActive ? "menuItem active" : "menuItem"}
+                            >
+                                <i className={menuItem.icon}></i>
+                                <p>{menuItem.menuName}</p>
+                        </NavLink>
                     )
                 })}
             </div>
+            
+            <div className="group">
+                <img src={Group} alt="" />
+            </div>
+        </div>
     )
 }
 
